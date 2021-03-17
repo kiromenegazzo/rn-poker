be14ce9e370aa5bdb60afcad36f0713379e1fd6b
Медянцев Кирилл Михайлович <kirill.medyantsev@domrf.ru>
2021-03-17 17:05:32 +0300

    fix colors

--- constants/colors.js
+++ constants/colors.js
@@ -3,4 +3,4 @@ export const greenDarkColor = '#2c6436';
 export const whiteColor = '#ffffff';
 export const blackColor = '#282828';
 export const redColor = '#e03c1b';
-export const redDarkColor = '#c93618';
+export const redDarkColor = '#9c2a13';
